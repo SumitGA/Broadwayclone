4aee45e7ed0794f7c893ae9fa1d2b39cc6ab9eb4
SumitG <sghost33@gmail.com>
2017-03-24 21:22:47 +0545

    adding reviews and modifying the view pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 	before_action :find_play
+	before_action :find_review, only: [:edit, :update, :destroy]
+
 
 	def new
 		@play = Play.find(params[:play_id])
@@ -19,6 +21,19 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def edit
+		
+	end
+
+	def update
+		
+		if @review.update(review_params)
+			redirect_to play_path(@play)
+		else
+			render 'edit'
+		end
+	end
+
 	private
 
 	def review_params
@@ -28,5 +43,10 @@ class ReviewsController < ApplicationController
 	def find_play
 		@play = Play.find(params[:play_id])
 	end
+
+	def find_review
+		@review = Review.find(params[:id])
+	end
+
 
 end
